fcf91652eeb77a2867cbe25e605e497d5e6b45d0
MikeNotu <mikebateman98@gmail.com>
2022-04-13 13:18:53 -0500

    Language Change buttons added

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,10 @@ export default function Header(){
     return(
         <div>
             <h1>{t("header.hello-world")}</h1>
+            <br/>
+            <br/>
+            <button onClick={()=>i18n.changeLanguage("es")}>ES</button>
+            <button onClick={()=>i18n.changeLanguage("en")}>EN</button>
         </div>
     )
 }
